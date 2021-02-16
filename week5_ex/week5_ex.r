5bce2a4db70c274edfdc6df17c5e318669dec2e5
alexkcode <alexiskwan.uiuc@gmail.com>
2021-02-15 22:37:39 -0600

    near end of assignment 5

--- week5_ex/week5_ex.r
+++ week5_ex/week5_ex.r
@@ -16,14 +16,7 @@ region <- read_csv("RegionData.csv")
 #
 ###########################
 
-migrant_0 <- 
-  pivot_longer(migrant,
-               c(tot_africa:tot_latam),
-               names_to = "region",
-               names_prefix = "tot_",
-               values_to = "origin_total",
-               names_transform = list(year = as.numeric)) %>%
-  left_join(region, by = c("CountryCode" = "countrycode"), suffix = c("_origin",""))
+migrant_0 <- left_join(migrant, region, by = c("CountryCode" = "countrycode"))
 
 # 1. What is the number of Migrant by region in 2015?
 
@@ -31,17 +24,17 @@ migrant_1 <-
   migrant_0 %>% 
   filter(year == 2015) %>%
   group_by(region) %>%
-  summarise(n = sum(origin_total, na.rm = TRUE))
+  summarise(n = sum(Migrants, na.rm = TRUE))
 migrant_1
 
 # region                      n
 # <chr>                   <dbl>
-# 1 Africa               16250082
-# 2 Asia                 62244538
-# 3 Developed countries 127072157
-# 4 Latam                 6174187
-# 5 Oceania                166414
-# 6 Territories/Others     470851
+# 1 Africa               20648953
+# 2 Asia                 72781741
+# 3 Developed countries 140393231
+# 4 Latam                 9024966
+# 5 Oceania                229158
+# 6 Territories/Others     622187
 
 # 2. How many countries have a share of immigrants that it’s greater than 
 # or equal to 50% of their population in 2015?
@@ -59,13 +52,13 @@ migrant_2
 # 3. In which world regions are most of these countries located?
 
 migrant_3 <-
-  migrant_2 %>%
+  migrant_0 %>%
   group_by(region) %>%
   count(region) %>%
   arrange(desc(n))
 migrant_3
 
-# Most of the countries from question 2 are in territorial regions.
+# Most of the countries are African countries.
 
 # 4. How many countries have a share of immigrants greater than or equal to 
 # 50% of their population if we consider the average from 1990 to 2015? 
@@ -103,6 +96,7 @@ migrant_5 <-
             by = c("CountryCode" = "CountryCode"),
             suffix = c("","_2015")) %>%
   mutate(refugee_diff = Refugees_2015 - Refugees_1990) %>%
+  distinct(Country, region, refugee_diff) %>%
   group_by(region) %>%
   summarise(refugee_diff_by_region = sum(refugee_diff, na.rm = TRUE)) %>%
   arrange(desc(refugee_diff_by_region))
@@ -110,21 +104,78 @@ migrant_5
 
 # region              refugee_diff_by_region
 # <chr>                                <dbl>
-# 1 Asia                            1605647250
-# 2 Territories/Others               855671250
-# 3 Oceania                           -1715250
-# 4 Developed countries              -50625000
-# 5 Latam                           -609931500
-# 6 Africa                         -1429246500
+# 1 Asia                               2140863
+# 2 Territories/Others                 1140895
+# 3 Oceania                              -2287
+# 4 Developed countries                 -67500
+# 5 Latam                              -813242
+# 6 Africa                            -1905662
 
 # Asia saw the largest net increase of refugees while Africa had the largest
-# exodus of refugees.
+# net decrease of refugees.
+
+migrant_5a <-
+  migrant_0 %>%
+  left_join(migrant_0 %>% filter(year == 1990) %>% select(Refugees, CountryCode), 
+            by = c("CountryCode" = "CountryCode"),
+            suffix = c("","_1990")) %>%
+  left_join(migrant_0 %>% filter(year == 2015) %>% select(Refugees, CountryCode), 
+            by = c("CountryCode" = "CountryCode"),
+            suffix = c("","_2015")) %>%
+  mutate(refugee_diff = Refugees_2015 - Refugees_1990) %>%
+  distinct(Country, region, refugee_diff)
+
+# Many of the differences seem to result in NAs seemingly because data was not
+# available or collected for earlier years. 
 
 # 6. Has there been any changes in the immigration patterns by gender over time? 
 # Which country has the lowest female immigration? 
 # Which region has the highest female immigration? 
 # Consider the average across all the years.
 
+migrant_6 <-
+  migrant_0 %>%
+  left_join(migrant_0 %>% filter(year == 1990) %>% select(Migrants, CountryCode), 
+            by = c("CountryCode" = "CountryCode"),
+            suffix = c("","_1990")) %>%
+  left_join(migrant_0 %>% filter(year == 2015) %>% select(Migrants, CountryCode), 
+            by = c("CountryCode" = "CountryCode"),
+            suffix = c("","_2015")) %>%
+  mutate(migrant_diff = Migrants_2015 - Migrants_1990) %>%
+  group_by(region) %>%
+  summarise(migrant_diff = mean(migrant_diff, na.rm = TRUE)) %>%
+  arrange(desc(migrant_diff))
+migrant_6
+
+# Developed countries have seen the largest increase of immigrants arriving from 
+# 1990 to 2015, while Territories and other types of countries have seen a very 
+# modest increase.
+
+migrant_6a <- 
+  migrant_0 %>%
+  group_by(year) %>%
+  summarise(avg = mean(Migrants, na.rm = TRUE))
+migrant_6a  
+
+# Every 5 years since 1990 immigration has increased by about 50k - 100k
+
+migrant_6b <- 
+  migrant_0 %>%
+  group_by(region) %>%
+  summarise(avg = mean(FemaleMigrants, na.rm = TRUE)) %>%
+  arrange(avg)
+migrant_6b
+  
+# Oceania has had the lowest average female immigration over the years.
+
+migrant_6c <- 
+  migrant_0 %>%
+  group_by(Country) %>%
+  summarise(avg = mean(FemaleMigrants, na.rm = TRUE)) %>%
+  arrange(desc(avg))
+migrant_6c
+
+# The United States has had the highest average female immigration over the years.
 
 ###########################
 #
@@ -135,12 +186,48 @@ migrant_5
 # 1. Use the is.na functions to calculate how many missing values in the 
 # refugees, population, and migrant columns.
 
+apply(is.na(migrant_0)[, c("Refugees", "Pop", "Migrants")],
+      MARGIN = 2,
+      FUN = sum)
+
+# Refugees Pop Migrants 
+# 366        0       15 
   
 # 2. For the column(s) with missing values, conduct some additional analysis - 
 # are those random by region, you think of an explanation of why there are 
 # these missing values? Would you classify these missing values as MCAR, MAR, or MNAR?
 
+refugees_by_year <- 
+  migrant_0 %>% 
+  mutate(Refugees_missing = case_when(is.na(Refugees) ~ 1, TRUE ~ 0)) %>% 
+  group_by(Country, region, year) %>% 
+  summarise(missing = sum(Refugees_missing))
+
+refugees_by_year %>% 
+  filter(missing > 0) %>%
+  group_by(region) %>% 
+  summarise(missing = sum(missing)) %>% 
+  arrange(desc(missing))
   
+# region              missing
+# <chr>                 <dbl>
+# 1 Territories/Others      156
+# 2 Africa                   54
+# 3 Latam                    48
+# 4 Asia                     42
+# 5 Oceania                  42
+# 6 Developed countries      24
+
+# Territories and Africa seem to have the most missing data. The countries in these
+# regions may not have the administrative infratstructure to collect such data
+# so it's likely that this missing data is not entirely randomly missing, or MNAR,
+
+refugees_by_year %>% 
+  group_by(Country) %>% 
+  summarise(missing = sum(missing)) %>% 
+  filter(missing > 0 & missing < 6)
+
 # 3. Write a short wrap-up of what you discovered on missing values. 
 # Note any surprising findings and you think missing values might affect any 
 # analysis contacted with the dataset.
+
